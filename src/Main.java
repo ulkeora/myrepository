ad836d282b137cb451248407b6796c4549b4b3b6
Aktan <aktan17720017@gmail.com>
2023-01-31 21:01:09 +0600

    first commit after delite branch

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+       master mr = new master();
+       mr.ad();
+    }
+}
---
+++ src/master.java
@@ -0,0 +1,6 @@
+public class master {
+    public void ad(){
+        System.out.println("gg dfdfdwp");
+
+    }
+}
